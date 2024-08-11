f3bbfa35e650122c42a5409ddae5c4c434c15bf9
Ayesha-Khan12 <developerayesha582@gmail.com>
2024-08-11 22:56:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var visitorName = "<b>JohnDoe</b>";
+var productTitle = "ordered";
+var quantity = "<b> 5 T-shirt(s)</b>";
+document.write(visitorName + " " + productTitle + " " + quantity + " " + "on XYZ clothing store");
+console.log(visitorName + " " + productTitle + " " + quantity + " " + "on XYZ clothing store");
+
+
